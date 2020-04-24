418aaa93c39092afb0a9147473d5412a5ac90f8b
BeginnerRudy <mrj9990123@gmail.com>
2020-04-24 11:45:45 +1000

    Turn on the auto debug mode of flask

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,8 @@
 """
-Author:      XuLin Yang
-Student id:  904904
+Author:      XuLin Yang & Renjie Meng
+Student id:  904904 & 877396
 Date:        2020-4-24 01:16:19
 Description: creates the application object as an instance of class Flask imported from the flask package.
 """
 
-from app import routes
-from app import resources
+from app import *
