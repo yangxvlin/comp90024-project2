4b27d15a4bb2e3aa6a3104286d3563af08d94317
BeginnerRudy <mrj9990123@gmail.com>
2020-04-24 14:08:27 +1000

    enable parse client GET json data

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,14 +1,71 @@
 """
-Author:      XuLin Yang
-Student id:  904904
-Date:        
+Author:      XuLin Yang & Renjie Meng
+Student id:  904904 & 877396
+Date:        2020-4-24 9:52:05
 Description: deprecated as we use ReSTFul design
 """
-
+from flask import request
+from flask import jsonify
+from flask import abort
 from app.resources import app
 
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+
+@app.route('/map', methods=['GET'])
+def get_tasks():
+    if not request.json or not "location" in request.json:
+        abort(400)
+    # task = {
+    #     'id': tasks[-1]['id'] + 1,
+    #     'title': request.json['title'],
+    #     'description': request.json.get('description', ""),
+    #     'done': False
+    # }
+    # tasks.append(task)
+    return jsonify({'task': request.json["location"]}), 201
+    # return jsonify({'tasks': 'yes'})
+
+# @app.route('/todo/api/v1.0/tasks', methods=['GET'])
+# def get_tasks():
+#     return jsonify({'tasks': tasks})
+
+
+# @app.route('/')
+# def index():
+#     return "The New World!"
+#
+#
+# @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+# def get_task(task_id):
+#     task = [task for task in tasks if task['id'] == task_id]
+#     if len(task) == 0:
+#         abort(404)
+#     return jsonify({'task': task[0]})
+
+#
+# @app.route('/todo/api/v1.0/tasks', methods=['POST'])
+# def create_task():
+#     if not request.json or not 'title' in request.json:
+#         abort(400)
+#     task = {
+#         'id': tasks[-1]['id'] + 1,
+#         'title': request.json['title'],
+#         'description': request.json.get('description', ""),
+#         'done': False
+#     }
+#     tasks.append(task)
+#     return jsonify({'task': task}), 201
