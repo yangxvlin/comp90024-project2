ec467362579f6bd5ef669f9587286c85347f4dd2
Xinyao Niu <morryniu123@gmail.com>
2020-04-27 12:48:33 +1000

    add file creation

--- crawler/gzip_reader.py
+++ crawler/gzip_reader.py
@@ -2,8 +2,7 @@ import gzip
 import json
 import io
 
-with gzip.open("./hydrated/coronavirus-tweet-id-2020-01-27-22.jsonl.gz", "rb") as f:
+with gzip.open("./hydrated/2020-01/coronavirus-tweet-id-2020-01-27-22.jsonl.gz", "rb") as f:
     with io.BufferedReader(f) as r:
         for line in r:
             print(json.loads(line)['place'])
-            exit(1)
