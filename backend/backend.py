48b59748697a478fc0d5ec7c3c0ba5144fcb6af7
yangxvlin <1768528843@qq.com>
2020-05-04 01:19:02 +1000

    add scenario 1 for AURIN

--- backend/backend.py
+++ backend/backend.py
@@ -16,11 +16,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-test', help='test the compilation of the application')
     parser.add_argument('-host', help='host address')
-    parser.add_argument('-post', help='host port number')
+    parser.add_argument('-port', help='host port number')
     args = parser.parse_args()
 
     if args.test:
         print("Successfully compiled")
         sys.exit(0)
 
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host=args.host, port=args.port)
