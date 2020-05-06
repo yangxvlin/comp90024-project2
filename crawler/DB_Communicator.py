e31fad7daacd475efa8b06ed55ebd3d815fc976d
BeginnerRudy <mrj9990123@gmail.com>
2020-05-06 18:49:38 +1000

    add couchdb

--- crawler/DB_Communicator.py
+++ crawler/DB_Communicator.py
@@ -1,3 +1,28 @@
+import couchdb
+import json
+
+# Environemt variable:
+# - couchdb host ip
+# - couchdb host port
+# - couchdb username
+# - couchdb password
+# - the couchdb database name
+# - the locatoin standards for mapping bounding box to location
+
+
+def bounding_box_to_location(data, standards=None):
+    # TODO: do nothing for now -> change later
+    return data
+
+def connect_to_coucbdb_servre(host, port, username, password):
+    secure_remote_server = couchdb.Server('http://' + username + ':' + password + '@' + host + ':' + port)
+    return secure_remote_server
+
+def create_databse(db_name, server):
+    return server.create(dbname)
+
+# data: the dictionary format of tweet object
 def send_to_db(data):
-    # finish this when db is defined
-    pass
+    preprocessed_tweet = bounding_box_to_location(data)
+    preprocessed_tweet["_id"] = "%d" % preprocessed_tweet["id"]
+    db.save(preprocessed_tweet)
