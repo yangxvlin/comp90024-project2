f0ef785c83a4d184d1e5b3056f088f829b38c63b
Mingyu <mingyusu9841@gmail.com>
2020-05-07 23:29:42 +1000

    Update map functions

--- backend/view_data.py
+++ backend/view_data.py
@@ -7,11 +7,11 @@ STATISTICS = {
   "language": "javascript",
   "views": {
     "city_hour_day": {
-      "map": "function (doc) {\n  if (doc.geo_code && doc.created_at) {\n    city = doc.geo_code;\n    date = new Date(doc.created_at);\n    day = (date.getDay() + 6) % 7;\n    emit([city, date.getHours(), day], date);\n  }\n}",
+      "map": "geo_codes = {\n  \"great_syd\": \"Greater Sydney\",\n  \"great_mel\": \"Greater Melbourne\",\n  \"great_brisbane\": \"Greater Brisbane\",\n  \"great_ald\": \"Greater Sydney\",\n  \"Australia other\": \"unknown\"\n}\n\nfunction (doc) {\n  city = geo_codes[doc.geo_code]\n\n  if (city && doc.created_at) {\n    date = new Date(doc.created_at);\n    day = (date.getDay() + 6) % 7;\n    emit([city, date.getHours(), day], date);\n  }\n}",
       "reduce": "_count"
     },
     "city_2020_month_day_hours": {
-      "map": "function (doc) {\n  date = new Date(doc.created_at);\n  year = date.getFullYear();\n  // Hour is ranged from 0 to 23\n  hour = date.getHours();\n  \n  if (doc.geo_code && year == 2020) {\n    if (hour >= 0 && hour <= 7) {\n      emit([doc.geo_code, year, date.getMonth(), date.getDay(), \"00:00-07:59\"], 1);\n    } else if (hour >= 8 && hour <= 15) {\n      emit([doc.geo_code, year, date.getMonth(), date.getDay(), \"08:00-15:59\"], 1);\n    } else if (hour >= 16 && hour <= 23) {\n      emit([doc.geo_code, year, date.getMonth(), date.getDay(), \"16:00-23:59\"], 1);\n    }\n  }\n}",
+      "map": "geo_codes = {\n  \"great_syd\": \"Greater Sydney\",\n  \"great_mel\": \"Greater Melbourne\",\n  \"great_brisbane\": \"Greater Brisbane\",\n  \"great_ald\": \"Greater Sydney\",\n  \"Australia other\": \"unknown\"\n}\n\nfunction (doc) {\n  date = new Date(doc.created_at);\n  year = date.getFullYear();\n  // Hour is ranged from 0 to 23\n  hour = date.getHours();\n  \n  city = geo_codes[doc.geo_code]\n  \n  if (city && year == 2020) {\n    if (hour >= 0 && hour <= 7) {\n      emit([city, year, date.getMonth(), date.getDay(), \"00:00-07:59\"], 1);\n    } else if (hour >= 8 && hour <= 15) {\n      emit([city, year, date.getMonth(), date.getDay(), \"08:00-15:59\"], 1);\n    } else if (hour >= 16 && hour <= 23) {\n      emit([city, year, date.getMonth(), date.getDay(), \"16:00-23:59\"], 1);\n    }\n  }\n}",
       "reduce": "_count"
     }
   }
@@ -28,24 +28,51 @@ get_view_url = "http://{username}:{password}@{host}:{port}/{database}/_design/{d
 
 # Date: 0-6 represents Sunday to Saturday
 req = requests.put(
-  update_design_doc_url.format(username=username, password=password, host=host, port=port, database=database, design_doc=design_doc),
+  update_design_doc_url.format(username=username, 
+                               password=password, 
+                               host=host, port=port, 
+                               database=database, 
+                               design_doc=design_doc),
   headers={"Content-Type": "application/json"},
   data=json.dumps(STATISTICS)
 )
 
 def get_city_hour_day(group_level=3):
   """
-  According to the group level provided by the user, return all the documents sorted in asending order from city, hour and day.
+  According to the group level provided by the user, return all the documents 
+  sorted in asending order from city, hour and day.
+
+  Parameters
+  ----------
+  group_level : int, optional
+    The number of levels to reduce the documents to
   """
   return json.loads(requests.get(
-    get_view_url.format(username=username, password=password, host=host, port=port, database=database, design_doc=design_doc, view_name="city_hour_day", group_level=group_level)
+    get_view_url.format(username=username, 
+                        password=password, 
+                        host=host, port=port, 
+                        database=database, 
+                        design_doc=design_doc, 
+                        view_name="city_hour_day", 
+                        group_level=group_level)
   ).content.decode("utf-8"))
 
 def get_city_2020_month_day_hours(group_level=5):
   """
-  According to the group level provided by the user, return all the documents created in 2020
-  sorted in asending order from city, month, day and hours.
+  According to the group level provided by the user, return all the documents 
+  created in 2020 sorted in asending order from city, month, day and hours.
+
+  Parameters
+  ----------
+  group_level : int, optional
+    The number of levels to reduce the documents to
   """
   return json.loads(requests.get(
-    get_view_url.format(username=username, password=password, host=host, port=port, database=database, design_doc=design_doc, view_name="city_2020_month_day_hours", group_level=group_level)
+    get_view_url.format(username=username, 
+                        password=password, 
+                        host=host, port=port, 
+                        database=database, 
+                        design_doc=design_doc, 
+                        view_name="city_2020_month_day_hours", 
+                        group_level=group_level)
   ).content.decode("utf-8"))
