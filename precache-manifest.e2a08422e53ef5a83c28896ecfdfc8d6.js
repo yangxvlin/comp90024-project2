b5ada8fd997e7d6c902018bb5dcc223873161dcb
Olivia0012 <lu.wang4@student.unimelb.edu.au>
2020-05-23 19:33:05 +1000

    Updates

---
+++ precache-manifest.e2a08422e53ef5a83c28896ecfdfc8d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9252265eb2239be7d3757c8eeada56f",
+    "url": "/relativepath/index.html"
+  },
+  {
+    "revision": "e9424f8f2533f900cb57",
+    "url": "/relativepath/static/css/2.6dc7c179.chunk.css"
+  },
+  {
+    "revision": "fe054d17932687adcda8",
+    "url": "/relativepath/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e9424f8f2533f900cb57",
+    "url": "/relativepath/static/js/2.a37949e4.chunk.js"
+  },
+  {
+    "revision": "067df07d627e2e9f014276dd5171ac8a",
+    "url": "/relativepath/static/js/2.a37949e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe054d17932687adcda8",
+    "url": "/relativepath/static/js/main.bb4302de.chunk.js"
+  },
+  {
+    "revision": "506bcd11f32294bcdd1d",
+    "url": "/relativepath/static/js/runtime-main.b47b7a27.js"
+  }
+]);
