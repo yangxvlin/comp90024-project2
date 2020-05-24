58372cff7e8a6687271d30b7c18c238fa9958da4
Mingyu <mingyusu9841@gmail.com>
2020-05-25 00:24:42 +1000

    Update views

--- backend/couchDBSetting.py
+++ backend/couchDBSetting.py
@@ -2,5 +2,6 @@ hosts = ["172.26.131.101", "172.26.131.74", "172.26.132.122"]
 port = "5984"
 username = "admin"
 password = "admin"
-live_db_name = "live-tweets"
+# live_db_name = "live-tweets"
 covid_db_name = "covid-19-tweets"
+all_tweets_db_name = "all-tweets"
